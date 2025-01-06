c778b7e3fcb26996f856c418d9d8596ed026c2c2
HayatNRT <hayatnrt7@gmail.com>
2025-01-06 17:48:59 +0530

    basics config added

--- src/main/java/com/nrt/redis/entities/Driver.java
+++ src/main/java/com/nrt/redis/entities/Driver.java
@@ -1,9 +1,6 @@
 package com.nrt.redis.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,4 +19,7 @@ public class Driver implements Serializable {
     private String lastName;
     private String email;
     private String phone;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "fk_location_id",referencedColumnName = "id")
+    private ExactLocation exactLocation;
 }
--- src/main/java/com/nrt/redis/entities/ExactLocation.java
+++ src/main/java/com/nrt/redis/entities/ExactLocation.java
@@ -1,9 +1,6 @@
 package com.nrt.redis.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,4 +13,8 @@ public class ExactLocation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private double latitude;
+    private double longitude;
+    @OneToOne(mappedBy = "exactLocation")
+    private Driver driver;
 }
